c65ba8fe4463eafa1004c3290980e612d69be5dc
Sarah Miller <smiller807@smith.edu>
2025-11-05 22:39:14 -0500

    finished phase 2

--- Library.java
+++ Library.java
@@ -1,12 +1,63 @@
 /* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
+public class Library extends Building{
 
-    public Library() {
+private Hashtable<String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>();
       System.out.println("You have built a library: 📖");
+    }
+
+    public void addTitle(String title){
+      if(collection.contains(title)){
+        System.out.println("This title is already in the collection");
+      } else {
+        collection.put(title,true);
+      }
+    }
+    public String removeTitle(String title){
+      if(collection.contains(title)){
+        collection.remove(title,true);
+      } else {
+        System.out.println("This title is not in the collection");
+
+      }
+      return title;
+    }
+    public void checkOut(String title){
+      if(collection.contains(title)){
+        collection.put(title,false);
+      }
+    }
+    public void returnBook(String title){
+      if(collection.contains(title)){
+        collection.put(title,true);
+      }
+    }
+    public boolean containsTitle(String title){
+      if(collection.contains(title)){
+          return true;
+      } else {
+        return false;
+      }
+    }
+    public boolean isAvailable(String title){
+      if (collection.containsKey(title)) { 
+        Boolean value = collection.get(title);
+        if (value == true) { 
+            return true;
+        }
+      }
+      return false;
+    }
+    public void printCollection(){
+      System.out.println(collection);
     }
   
     public static void main(String[] args) {
-      new Library();
+      Library newLibrary = new Library("Neilson Library", "Seeyle Lawn", 4);
     }
   
   }
