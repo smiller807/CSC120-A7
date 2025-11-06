dd41b09e8d4f569dec30ec5d30ded8dce3c2da65
Sarah Miller <smiller807@smith.edu>
2025-11-05 22:05:37 -0500

    Finished phase 1

--- House.java
+++ House.java
@@ -1,12 +1,59 @@
 /* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
+public class House extends Building {
 
-  public House() {
+  private ArrayList<Student> residents;
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<Student>();
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
   }
 
+  public boolean hasDiningRoom(){
+    if (this.hasDiningRoom = true){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public int nResidents(){
+    int nResidents = residents.size();
+    return nResidents;
+  }
+
+  public void moveIn(Student s){
+    if (residents.contains(s)){
+      System.out.println("This Student is already moved in");
+    } else {
+      residents.add(s);
+    }
+  }
+
+  public Student moveOut(Student s){
+    if (residents.contains(s)){
+      residents.remove(s);
+    } else {
+      System.out.println("This Student is already moved in out");
+    }
+    return s;
+  }
+
+  public boolean isResident(Student s){
+    if (residents.contains(s)){
+      System.out.println("This student is a resident");
+      return true;
+    } else {
+      System.out.println("This student is not a resident");
+      return false;
+    }
+  }
+
   public static void main(String[] args) {
-    new House();
+    House myHouse = new House("Albright House", "7 Bedford Terrace", 4, false);
   }
 
 }
